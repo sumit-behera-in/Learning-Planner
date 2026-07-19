f7131a250f25b76adba84ccdd0bfec268a969f22
sumit-behera-in <sumitbehera1508@gmail.com>
2026-07-19 17:55:58 +0530

    Update .gitignore, add resource shrinking, and modify AndroidManifest.xml permissions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,7 @@ android {
 
     buildTypes {
         release {
+            isShrinkResources = true
             isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
